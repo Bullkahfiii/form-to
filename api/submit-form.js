9dc4ce8e3b36231c4a65eca15d8b896c09cbe42a
Bullkahfiii <bul.kahfi1@gmail.com>
2025-06-26 17:43:39 +0700

    submit-form.js

--- api/submit-form.js
+++ api/submit-form.js
@@ -1,20 +1,22 @@
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        return res.status(405).json({ message: 'Method not allowed' });
-    }
+  const GAS_URL = 'https://script.google.com/macros/s/AKfycbyk8yPsU5lD9RPZG1COgI4-tgVWi-Z8cCxzenlgABxCfV1p2hmxaWPvxx-3aLVwkERB/exec'; // Ganti dengan URL deployment Anda
 
-    try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbyk8yPsU5lD9RPZG1COgI4-tgVWi-Z8cCxzenlgABxCfV1p2hmxaWPvxx-3aLVwkERB/exec', {
-            method: 'POST',
-            body: JSON.stringify(req.body),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        
-        const data = await response.json();
-        return res.status(200).json(data);
-    } catch (error) {
-        return res.status(500).json({ error: 'Internal server error' });
-    }
+  try {
+    const response = await fetch(GAS_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': req.headers['content-type'] // Pertahankan content-type asli
+      },
+      body: req.body
+    });
+    
+    const result = await response.json();
+    return res.status(response.status).json(result);
+    
+  } catch (error) {
+    return res.status(500).json({
+      error: 'Internal server error',
+      details: error.message
+    });
+  }
 }
